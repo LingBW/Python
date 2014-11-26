a7a0c5b96f8b19fbfbb36ec17e9018e9746a2677
Bingwei Ling <ufwrgw@gmail.com>
2014-11-26 13:54:03 -0500

    Update track.py

--- track.py
+++ track.py
@@ -2,7 +2,8 @@
 This routine compares MULTIPLE drifter tracks to MULTIPLE model-derived tracks
 It is a enhanced version of Jian's track_cmp.py routine as modified by Conner Warren in summer 2014.
 Many of the functions and variables were renamed to better reflect their tasks and identity. 
-Some comments and adjustments by JiM.
+Some comments and adjustments by JiM and Bingwei.
+In November 2014, we added the option to run the code backwards
 
 GENERAL NOTES:
     1. Hardcodes are at the beginning of the program or function
@@ -21,101 +22,127 @@ import pytz
 import pandas as pd
 sys.path.append('../bin')
 import netCDF4 
-from track_functions import get_drifter, get_fvcom, get_roms, draw_basemap, distance # all homegrown functions needed for this routine
+from track_functions import get_drifter,get_fvcom,get_roms,draw_basemap,distance,uniquecolors
+from makegrid import clickmap, points_between
+st_run_time = datetime.now()
 
-# Step 2: Hardcode constants
-# some of the drifters apparently test by Conner
-#drifter_ids = ['115410701','118410701','108410712','108420701','110410711','110410712','110410713','110410714',
-#               '110410715','110410716','114410701','115410701','115410702','119410714','135410701','110410713','119410716']                                                  # Default drifter ID
-drifter_ids = ['147420706']  # ['147420706', '146410702','148410723', '148410701','148410727', '148410729'] ['138410721']
-MODE = 'HINDCAST'            # 'FORECAST' or 'HIHDCAST'
-INPUT_DATA = 'drift_X.dat'   # if raw data, use "drift_X.dat";if want to get drifter data in database, use "None" 
+########## Step 1: Options ###########################
+'''Three options to define track start 1) by drifters;
+   2) user defined along a straight line(s); 3) clicking map'''
+option=1
+DAYS = 2                     # Length of time wanted in track
+run_time = datetime(2013,6,20,12,0,0,0,pytz.UTC) 
+MODEL = 'FVCOM'              # 'FVCOM', 'ROMS' or 'BOTH'
 DEPTH = -1.                  # depth of drogue in meters
-# starttime = datetime(2011,5,12,13,0,0,0,pytz.UTC)
-starttime = None             # If it's None, use the last drifter time.
-DAYS = 1                     # Number or None. Length of time wanted in track, if not given, track to the last poistion of drifter.
-MODEL = 'ROMS'               # 'FVCOM', 'ROMS' or 'BOTH'
-GRID = 'GOM3'                # '30yr', 'GOM3' or 'massbay'(both 'GOM3' and 'massbay' are forecast), only used in fvcom.
-
-for ID in drifter_ids:
-    print "ID: ", ID
-    drifter = get_drifter(ID, INPUT_DATA)# New drifter data or old drifter data
-    points_drifter = drifter.get_track(starttime,DAYS)
-    if MODE == 'HINDCAST':
-        if MODEL in ("FVCOM", "BOTH"):
-            assert GRID=="30yr"
-        starttime = points_drifter['time'][0]
-        endtime = points_drifter['time'][-1]
-        lon, lat = points_drifter['lon'][0], points_drifter['lat'][0]
-    elif MODE == 'FORECAST':
-        # adjust for the added 4 hours in the models
-        time1 = pytz.utc.localize(datetime.now().replace(hour=0,minute=0))-timedelta(days=3)
-        # get starttime, and lon, lat
-        if starttime:
-            if starttime < time1:
-                raise Exception('start time should be later than the time that 3days before today.')
-            l1 = points_drifter['time']-points_drifter['time'][0]
-            l2 = starttime - points_drifter['time'][0]
-            index = np.where(abs(l1-l2)==min(abs(l1-l2)))[0][0]
-            lon, lat = points_drifter['lon'][index], points_drifter['lat'][index]
-        else:
-            starttime = points_drifter['time'][-1]
-            if starttime < time1:
-                raise Exception('starttime should be later than the time that 3days before today, drifter is too old')
-            lon, lat = points_drifter['lon'][-1], points_drifter['lat'][-1]
-        # get endtime
-        if DAYS:
-            endtime = starttime + timedelta(days=DAYS)
-        else:
-            endtime = starttime + timedelta(days=3)
-    # read data points from fvcom and roms websites and store them
-
-    #set latitude and longitude arrays for basemap
-    lonsize = [min(points_drifter['lon']), max(points_drifter['lon'])]
-    latsize = [min(points_drifter['lat']), max(points_drifter['lat'])]
-    diff_lon = (lonsize[0]-lonsize[1])*4 # leave space in sides
-    diff_lat = (latsize[1]-latsize[0])*4
-    lonsize = [lonsize[0]-diff_lon,lonsize[1]+diff_lon]
-    latsize = [latsize[0]-diff_lat,latsize[1]+diff_lat]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    draw_basemap(fig, ax, lonsize, latsize)
-    ax.plot(points_drifter['lon'],points_drifter['lat'],'ro-',label='drifter')
-    ax.plot(points_drifter['lon'][0],points_drifter['lat'][0],'c.',label='Startpoint',markersize=20)
-    if MODEL in ('FVCOM', 'BOTH'):
-        get_fvcom_obj = get_fvcom(GRID)
-        url_fvcom = get_fvcom_obj.get_url(starttime, endtime)
-        points_fvcom = get_fvcom_obj.get_track(lon,lat,DEPTH,url_fvcom)           # iterates fvcom's data
-        dist_fvcom = distance((points_drifter['lat'][-1],points_drifter['lon'][-1]),(points_fvcom['lat'][-1],points_fvcom['lon'][-1]))
-        print 'The separation of fvcom was %f kilometers for drifter %s' % (dist_fvcom[0], ID )
-        ax.plot(points_fvcom['lon'],points_fvcom['lat'],'yo-',label='fvcom')
+#OUTPUTDIR='/net/nwebserver/epd/ocean/MainPage/track/ccbay/'
+png_dir = '/home/bling/Documents/track-figures/'
+points = {'lats':[],'lons':[]}
+###########  Step 2:Check more detail#########################
+if option==1: # specified drifters
+    # Default driftecr ID
+    MODE = 'FORCAST'           # 'FORECAST' or 'HINDCAST'
+    GRID = 'massbay'            # '30yr', 'massbaya', 'GOM3a', 'GOM3' or 'massbay'
+    drifter_ids = ['115410701','118410701','108410712','108420701','110410711','110410712',
+                   '110410713','110410714','110410715','110410716','114410701','115410701',
+                   '115410702','119410714','135410701','110410713','119410716']  
+elif option==2: # user specified pts
+    track_way = 'forwards' # two options: forwards,backwards
+    GRID = 'massbaya'     # '30yr', 'massbaya', 'GOM3a', 'GOM3' or 'massbay'
+    (lat,lon)=points_between((41.729686, -70.636645), (41.686903, -70.665452),1)
+    (lat2,lon2)=points_between((41.654465, -70.657109), (41.661779, -70.736143),1)
+    #(lat3,lon3)=points_between((41.687952, -70.695588), (41.665847, -70.677392),0)   
+    lat=lat+lat2#+lat3
+    lon=lon+lon2#+lon3
+    print 'You added %d points.' % len(lat)
+elif option==3: # click on a map
+    track_way = 'forwards' # two options: forwards,backwards
+    GRID = 'massbaya'     # '30yr', 'massbaya', 'GOM3a', 'GOM3' or 'massbay'
+    numpts=9 # at most added on the map
+    [lon,lat]=clickmap(numpts) # gets lat/lon by clicking map
+    print 'You added %d points.' % len(lat)
+##################Step 3: executing ##################   
+if option==1:
+    drifter_ID = '140410701'
+    # if raw data, use "drift_X.dat";if want to get drifter data in database, use "None"
+    INPUT_DATA = 'drift_X.dat'   
+    start_time = datetime(2014,11,26,11,50,0,0,pytz.UTC)
+    end_time = start_time + timedelta(DAYS)
+    drifter = get_drifter(drifter_ID, INPUT_DATA)# New drifter data or old drifter data
+    dr_points = drifter.get_track(start_time,DAYS)
+    st_point = (dr_points['lon'][-1],dr_points['lat'][-1])
+    if MODEL in ('FVCOM','BOTH'):
+        get_obj = get_fvcom(GRID)
+        url_fvcom = get_obj.get_url(start_time,end_time)
+        point = get_obj.get_track(st_point[0],st_point[1],DEPTH,url_fvcom)
     if MODEL in ('ROMS', 'BOTH'):
-        get_roms_obj = get_roms()
-        url_roms = get_roms_obj.get_url(starttime, endtime)
-        points_roms = get_roms_obj.get_track(lon, lat, DEPTH, url_roms)
-        if type(points_roms['lat']) == np.float64:                             # ensures that the single point case still functions properly   
-            points_roms['lon'] = [points_roms['lon']] 
-            points_roms['lat'] = [points_roms['lat']]
-        #Calculate the distance separation
-        dist_roms = distance((points_drifter['lat'][-1],points_drifter['lon'][-1]),(points_roms['lat'][-1],points_roms['lon'][-1]))
-        print 'The separation of roms was %f kilometers for drifter %s' % (dist_roms[0], ID)
-        ax.plot(points_roms['lon'],points_roms['lat'], 'go-', label='roms')
-
-    '''
-    #Plot the drifter track, model outputs form fvcom and roms, and the basemap          
+        get_obj = get_roms()
+        url_roms = get_obj.get_url(start_time, end_time)
+        point = get_obj.get_track(st_point[0],st_point[1],DEPTH,url_roms)
+    points['lats'].extend(point['lat'])
+    points['lons'].extend(point['lon'])
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    draw_basemap(fig, ax, lonsize, latsize)
-    ax.plot(points_drifter['lon'],points_drifter['lat'],'ro-',label='drifter')
-    ax.plot(points_fvcom['lon'],points_fvcom['lat'],'yo-',label='fvcom')
-    ax.plot(points_roms['lon'],points_roms['lat'], 'go-', label='roms')
-    '''
-    # ax.plot(points_drifter['lon'][0],points_drifter['lat'][0],'c.',label='Startpoint',markersize=20)
-    plt.title('ID: {0}, {1} to {2}'.format(ID, starttime.strftime("%m/%d/%Y %H:%M"), endtime.strftime("%m/%d/%Y %H:%M")))
+    ax.plot(point['lon'],point['lat'],'ro-',label='drifter')
+    ax.plot(dr_points['lon'][-1],dr_points['lat'][-1],'c.',label='Startpoint',markersize=20)
+    ax.annotate(drifter_ID, xy=(st_point),fontsize=6,xytext=(st_point[0]+0.03,st_point[1]+0.01),
+                arrowprops=dict(arrowstyle="wedge")) #facecolor=colors[i]
+    draw_basemap(fig, ax, points)
+    plt.title('{0} {1}-days from {2} '.format(MODE,DAYS,start_time.strftime("%F %H:%M")))
     plt.legend(loc='lower right')
-    plt.xlabel('Longitude')
-    plt.ylabel('Latitude')
-    plt.show()
-    # plt.savefig('plots/'+MODEL+str(ID)+'.png')
-    plt.savefig('ID-' + str(ID) +'-%s.png' % MODE, dpi=200)
+    plt.savefig(png_dir+"Drifter_track: %s"%(drifter_ID),dpi=200) #bbox_inches='tight'   
+############################
+if (option==2) | (option==3):
+    obj_points = {'lat':[41.801167,41.807005],'lon':[-70.529587,-70.534930]}
+    points['lats'].extend(obj_points['lat']); points['lons'].extend(obj_points['lon'])
+    colors=uniquecolors(len(lat)) #config colors
+    fig = plt.figure(figsize=(16,9))
+    ax1 = fig.add_subplot(231); ax2 = fig.add_subplot(232); ax3 = fig.add_subplot(233)
+    ax4 = fig.add_subplot(234); ax5 = fig.add_subplot(235); ax6 = fig.add_subplot(236)
+    loax = [ax1,ax2,ax3,ax4,ax5,ax6]
+    for j in range(6):   
+        ax = loax[j]
+        start_time = run_time + timedelta(j) 
+        end_time = start_time + timedelta(DAYS)
+        if track_way == 'backwards':
+            end_time = run_time + timedelta(j) 
+            start_time = end_time + timedelta(DAYS)
+        p_file = open('ax%d_points.txt'%(j+1),'w')
+        for i in range(len(lat)):
+            print "ax{0}: {1}th point starting at {3} {2} for {4}days".format((j+1),(i+1),track_way,start_time.strftime("%D"),DAYS)
+            stp_ID = 'Start%d' % (i+1)
+            if MODEL in ('FVCOM','BOTH'):
+                get_obj = get_fvcom(GRID)
+                url_fvcom = get_obj.get_url(start_time,end_time)
+                point = get_obj.get_track(lon[i],lat[i],DEPTH,url_fvcom)
+            if MODEL in ('ROMS', 'BOTH'):
+                get_obj = get_roms()
+                url_roms = get_obj.get_url(start_time, end_time)
+                point = get_obj.get_track(lon[i],lat[i],DEPTH,url_roms)
+            
+            points['lats'].extend(point['lat'])
+            points['lons'].extend(point['lon'])
+            #for p in point['lon']: points['lons'].append(p)
+            for h in zip(point['lat'],point['lon']): p_file.write('%f,%f\n'%h)
+            #Calculate the distance separation
+            dist = distance((lat[i],lon[i]),(point['lat'][-1],point['lon'][-1]))    
+            print 'The separation of %s is %f kilometers to %s'%(track_way,dist[0],stp_ID)
+            ax.plot(point['lon'],point['lat'], '-',color=colors[i], linewidth=2)
+            ax.annotate(stp_ID, xy=(lon[i],lat[i]),fontsize=6,xytext=(lon[i]+0.03,
+                        lat[i]+0.01),arrowprops=dict(arrowstyle="wedge",facecolor=colors[i])) 
+                        # connectionstyle="angle3,angleA=0,angleB=-90"        
+        ax.plot(obj_points['lon'],obj_points['lat'],'c.',markersize=8)
+        ax.text(obj_points['lon'][0],obj_points['lat'][0],'Trash',fontsize=8,rotation=0)
+        #plt.legend(loc='lower right')
+        '''if i==3 or i==4 or i==5:
+            ax.set_xlabel('longitude')
+        if i==0 or i==3:
+            ax.set_ylabel('latitude')'''
+        ax.set_title("%s: %s - %s"%(track_way,start_time.strftime("%m/%d/%Y"),end_time.strftime("%m/%d/%Y")))
+        p_file.close()                            
+    for ax in loax:
+        draw_basemap(fig, ax, points)
+    plt.savefig(png_dir+"%s_%s-days_from_Buzzards_Bay"%(track_way,DAYS),dpi=400) #,bbox_inches='tight'
+##################################################################
+en_run_time = datetime.now()
+print 'Take '+str(en_run_time-st_run_time)+' running the code. End at '+str(en_run_time)
+plt.show()
